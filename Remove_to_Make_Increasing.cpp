d8b71d294a28e1bf2ff01512e0f549e9aa842759
Akshath Pandey <95563805+anonymousknight07@users.noreply.github.com>
2023-04-24 21:51:01 +0530

    Add files via upload
    Day-2 of 100

---
+++ Remove_to_Make_Increasing.cpp
@@ -0,0 +1,54 @@
+// 1909. Remove One Element to Make the Array Strictly Increasing
+// Given a 0-indexed integer array nums, return true if it can be made strictly increasing after removing exactly one element, or false otherwise. If the array is already strictly increasing, return true.
+// The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1 <= i < nums.length).
+// Example 1:
+
+// Input: nums = [1,2,10,5,7]
+// Output: true
+// Explanation: By removing 10 at index 2 from nums, it becomes [1,2,5,7].
+// [1,2,5,7] is strictly increasing, so return true.
+// Example 2:
+
+// Input: nums = [2,3,1,2]
+// Output: false
+// Explanation:
+// [3,1,2] is the result of removing the element at index 0.
+// [2,1,2] is the result of removing the element at index 1.
+// [2,3,2] is the result of removing the element at index 2.
+// [2,3,1] is the result of removing the element at index 3.
+// No resulting array is strictly increasing, so return false.
+// Example 3:
+
+// Input: nums = [1,1,1]
+// Output: false
+// Explanation: The result of removing any element is [1,1].
+// [1,1] is not strictly increasing, so return false.
+ 
+class Solution {
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        int rcount=0,lcount=0;
+        int last=nums.back();
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]<last){
+                last=nums[i];
+            }
+            else{
+                rcount+=1;
+            }
+
+        }
+        last=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>last){
+                last=nums[i];
+            }
+            else{
+                lcount++;
+            }
+        }
+        return min(lcount,rcount)<=1;
+        
+    }
+};
+ 
